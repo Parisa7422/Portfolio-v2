beeb33dda43231edc908fc24c1c409a7c9e1110f
Parisa Taheri <parisa7422@gmail.com>
2024-11-11 16:34:04 +0100

    Deploy to gh-pages branch

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="d-flex flex-column align-items-center mb-4">
-      <hr className="w-75" />
-      <span>Design by ME</span>
-    </div>
-  );
-}
-export default Footer;
