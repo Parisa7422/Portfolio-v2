5687675e8a1087e87b40124aa53a8ad6045b1d9c
Parisa <parisa7422@gmail.com>
2024-11-11 16:21:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-v2/", // Replace with the name of your repository
+  base: "/Portfolio-v2/",
 });
